88280a09e1f13fbf36608f13ccd5a0b85a34226d
tchaari <tarak.chaari@gmail.com>
2013-12-13 12:31:58 +0100

    Set ondemand, deadline and bln off as defaults
    Change-Id: I4d2cba3fbbbcb1665e6846c86505569a810b77ee

--- drivers/misc/bln.c
+++ drivers/misc/bln.c
@@ -18,7 +18,7 @@
 #include <linux/timer.h>
 #include <linux/wakelock.h>
 
-static bool bln_enabled = true; /* is BLN function is enabled */
+static bool bln_enabled = false; /* is BLN function is enabled */
 static bool bln_ongoing = false; /* ongoing LED Notification */
 static bool bln_suspended = false; /* is system suspended */
 static struct bln_implementation *bln_imp = NULL;
